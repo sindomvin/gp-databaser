34c6fbddc30204d87b6d90289f356d996626b003
sindomvin <115994697+sindomvin@users.noreply.github.com>
2023-04-05 16:27:15 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,18 @@
 //Database
 var database = [
     {
+        name: "Rækkefølge bestemmer",
+        type: "Artikel",
+        description: "om Gp",
+        theme: ["Banker", "Klima"],
+        rating: 0      
+    },
+    {
         name: "Artikel om Gp 3",
         type: "Artikel",
         description: "om Gp",
         theme: ["Bank", "Penge"],
-        rating: 4
+        rating: 3
         // andre ideer:
         //    nivau
         //    forfatter
@@ -70,7 +77,7 @@ var database = [
         price: 0.00,
         date: "2021",
         theme: ["Pengesystem", "Pengeskabelse", "Økonomiske bobler", "English"],
-        description: "It is widely acknowledged that the build-up of a financial bubble coincides with and may even be caused by excessive bank credit or lending (e.g., Reinhart and Rogoff, 2009; Kindleberger and Aliber, 2011; Jordà, Schularick and Taylor, 2015). Equally well understood, at least to the readers of this journal, is the fact that commercial bank lending involves money creation (Werner, 1997, 2014a, b; Ryan-Collins et al., 2011; McLeay et al., 2014; Jakab and Kumhoff, 2019).",
+        description: "It is widely acknowledged that the build-up of a financial bubble coincides with and may even be caused by excessive bank credit or lending (e.g., Reinhart and Rogoff, 2009; Kindleberger and Aliber, 2011; Jordà, Schularick and Taylor, 2015). Equally well understood, at least to the readers of this journal, is the fact that commercial bank lending involves money creation (Werner, 1997, 2014a, b; Ryan-Collins et al., 2011; McLeay et al., 2014; Jakab and Kumhoff, 2019).",
         link:"https://static1.squarespace.com/static/587382daebbd1a717d8e9df3/t/6169beeeaa430d291d07932d/1634320111221/How_financial_bubbles_are_fueled_Ib_Ravn.pdf",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/1634985684931-GQ8880SKK8PEIC1QLHYU/2markus-spiske-qR-Dj7c2ilk-unsplash.jpg?format=500w"
     },
@@ -84,5 +91,5 @@ var database = [
         description: "Vi tror, vi ved, hvordan banker fungerer – og at systemet er stabilt. Men nyere forskning peger på, at bankernes forunderlige evne til at skabe penge gennem udlånet pumper priserne på fx boligmarkedet op, indtil bankerne og somme tider hele systemet falder sammen. Derfor er det elementært demokratisk behov, at vi får en bedre forståelse af, hvordan penge skabes – og hvordan de er med til at skabe boligbobler og finanskriser.",
         link: "https://www.godepenge.dk/analyser/2021/10/17/banker-lner-ikke-bare-penge-ud-de-skaber-dem",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/1634465043055-HO11FDBTGRUKR7ZVIM7V/photo-1501167786227-4cba60f6d58f.jpg?format=500w"
-    },
+    }
 ];
