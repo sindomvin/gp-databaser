8122cc2c4438ae2586f825c1333bf2d1ad3cef9f
sindomvin <115994697+sindomvin@users.noreply.github.com>
2024-05-09 12:01:43 +0200

    Update 0.1.database.js

--- 0.1.database.js
+++ 0.1.database.js
@@ -99,4 +99,18 @@ var database = [
         link: "https://www.godepenge.dk/analyser/2021/11/4/hvis-du-ikke-hopper-p-boligstigen-bliver-du-krt-over-af-bankernes-pengemaskine",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/01d40260-a092-4e7d-a2f7-9bc740c24666/unsplash-image-ifeqg7RgkiA.jpg?format=1000w"
     },
+    {
+        //Et eksempel
+        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
+        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
+        rating: 10, //uden anførselstegn
+        price: 0.00, //med 2 decimaler, uden anførselstegn
+        date: "2021-11-29", //med anførselstegn
+        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
+        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
+        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
+        //kun linje skift der hvor der står "<br>"
+        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
+        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
+    },
 ];
