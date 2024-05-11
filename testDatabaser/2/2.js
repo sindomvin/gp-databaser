dafa1c2872aad34ba803793ae3990167e8a85377
2testonlinebib <2testonlinebib@proton.me>
2024-05-11 13:11:23 +0200

    Update 2.js
    lavet ny anbefaling af artiklen skrevet af Simons gode ven

--- testDatabaser/2/2.js
+++ testDatabaser/2/2.js
@@ -66,4 +66,18 @@ var database = [
         link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
     },
+    {
+        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
+        //udfyld derefter med info for referencen
+        name: 'Simons gode ven', //navn
+        type: 'Artikel', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
+        rating: 5, //kvaliteten, tal mellem 1-10
+        price: 0.00, //Pris for at tilgå referencen
+        date: '2021-11-29', //tilblivesesdato for det man referer til, år-månded-dag
+        authors: 'Simons gode ven', //forfatter eller forfattere
+        theme: ['Demokrati', 'Pengeskabelse', 'EU', 'Centralbanker'], //Emnerne på denne form ['Emne1', 'Emne2', 'Emne3']. Det skal være med stort forbogstav
+        description: 'En rigtig god artikel af Simon ven', //Beskrivelse af den specifikke Artikel, bog osv.
+        link: "https://www.godepenge.dk", //link 
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbt.bmcdn.dk%2Fmedia%2Fcache%2Fresolve%2Fimage_1240%2Fimage%2F72%2F728652%2F6899368-bt.jpg&f=1&nofb=1&ipt=b47bdbea78d1d982148fb7d17dd56f842027a5b0219bbe53bb6749321f24dcb7&ipo=images", //link til billede
+    },
 ];
