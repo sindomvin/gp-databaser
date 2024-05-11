537bfd6c27e1e55e32625a2fa995b09ca9b7f77a
chrgodskesen <chrgodskesen@gmail.com>
2024-05-11 16:02:30 +0200

    Update 3.js

--- testDatabaser/3/3.js
+++ testDatabaser/3/3.js
@@ -66,4 +66,18 @@ var database = [
         link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
     },
+     {
+        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
+        //udfyld derefter med info for referencen
+        name: 'Debunking Economics: The Naked Emperor Dethroned?', //navn
+        type: 'Bog', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
+        rating: 6, //kvaliteten, tal mellem 1-10
+        price: 350.00, //Pris for at tilgå referencen
+        date: '2011-01-01', //tilblivesesdato for det man referer til, år-månded-dag
+        authors: 'Steve Keen', //forfatter eller forfattere
+        theme: ['Finanskrise', 'Boligmarkedet', 'English'], // 'Økonomikritik' Hvis det bliver accepteret som emne
+        description: 'Denne bog forklarer i detaljer, hvad der er galt med det meste af den økonomiske videnskab, og hvorfor de fleste økonomer ikke forudså den globale finanskrise i 2007-08. Professor i økonomi Steve Keen er kendt for at være én af de få, der på forhånd advarede om, at en økonomisk krise ville bryde ud. </i>Debunking Economics</i> forklarer alle de urealistiske antagelser og logiske og matematiske fejl, som moderne økonomisk videnskab bygger på - i et indførende og ikke-matematisk sprog. Emnerne er komplekse, men man behøver ikke at have læst økonomi eller forstå ligninger for at kunne følge med. Teorien om at markedet balancerer udbud og efterspørgsel og finder et prisniveau, der fører til økonomisk ligevægt og vækst, er forfejlet. Den kan ikke en gang give en forklaring på økonomiske kriser. Steve Keen giver også sit bud på hvordan en realistisk økonomisk videnskab kunne se ud, hvis den tog højde for gældens og pengeskabelsens betydning for økonomiske op- og nedture.', //Beskrivelse af den specifikke Artikel, bog osv.
+        link: "https://www.saxo.com/dk/debunking-economics_professor-steve-keen-steve-keen_epub_9781848139954", //link 
+        image: "https://imgcdn.saxo.com/_9781848139954/0x500", //link til billede
+    },
 ];
