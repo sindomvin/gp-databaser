d82a4ac561a6e80c9afa0dbfddf05cfc6b24b000
ibravn <ravn@dpu.dk>
2024-05-11 17:30:36 +0200

    Update 4.js

--- testDatabaser/4/4.js
+++ testDatabaser/4/4.js
@@ -53,7 +53,6 @@ var database = [
         image: "", //link til billede
     },
     {
-        //Et eksempel
         name: "New Paradigm in Macroeconomics", //Bemærk det er skrevet mellem anførselstegnene
         type: "Bog", //Kun et medie og skrevet med stort forbogstav
         rating: 8, //uden anførselstegn
@@ -67,8 +66,7 @@ var database = [
         image: "https://link.springer.com/book/10.1057/9780230506077",
     },
 {
-        //Et eksempel
-        name: "Penge som tryllestøv: Hvordan banker skaber penge og blæser finansbobler op", //Bemærk det er skrevet mellem anførselstegnene
+         name: "Penge som tryllestøv: Hvordan banker skaber penge og blæser finansbobler op", //Bemærk det er skrevet mellem anførselstegnene
         type: "Bog", //Kun et medie og skrevet med stort forbogstav
         rating: 10, //uden anførselstegn
         price: 200.00, //med 2 decimaler, uden anførselstegn
@@ -80,8 +78,6 @@ var database = [
         link: "https://www.godepenge.dk/shop",
         image: "Stockphoto bog",
     }, {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
         name: 'The veil of deception over money: How central bankers and textbooks distort the nature of banking and central banking', //navn
         type: 'Artikel', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
         rating: 6, //kvaliteten, tal mellem 1-10
@@ -93,8 +89,6 @@ var database = [
         link: "https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=http://www.paecon.net/PAEReview/issue63/Haring63.pdf&ved=2ahUKEwjdmKqD6IWGAxVeFhAIHWJ1DnAQFnoECBAQAQ&usg=AOvVaw1IEm61pQkwzYn5RmXApcEb", //link 
         image: "Stockphoto artikel", //link til billede
     }, {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
         name: 'Between debt and the devil: Money, credit, and fixing global finance', //navn
         type: 'Bog', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
         rating: 5, //kvaliteten, tal mellem 1-10
@@ -106,21 +100,6 @@ var database = [
         link: "https://press.princeton.edu/books/hardcover/9780691169644/between-debt-and-the-devil", //link 
         image: "Stockphoto bog", //link til billede
     }, {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
-        name: 'Skabelon for en reference', //navn
-        type: 'Artikel', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
-        rating: 3, //kvaliteten, tal mellem 1-10
-        price: 0.00, //Pris for at tilgå referencen
-        date: '2021-11-29', //tilblivesesdato for det man referer til, år-månded-dag
-        authors: 'forfatter', //forfatter eller forfattere
-        theme: ['Demokrati', 'Pengeskabelse'], //Emnerne på denne form ['Emne1', 'Emne2', 'Emne3']. Det skal være med stort forbogstav
-        description: 'Obejkt til kopiering', //Beskrivelse af den specifikke Artikel, bog osv.
-        link: "https://www.godepenge.dk", //link 
-        image: "", //link til billede
-    }, {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
         name: 'Princes of the Yen', //navn
         type: 'Bog', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
         rating: 4, //kvaliteten, tal mellem 1-10
@@ -132,9 +111,7 @@ var database = [
         link: "https://quantumpublishers.com/uk-cart/index.php?route=product/product&product_id=50", //link 
         image: "Stockphoto bog", //link til billede
     }, {
-        //skabelon, kopier fra "{" linjen over denne til "}," indsæt så efter "}," for at lave en ny reference
-        //udfyld derefter med info for referencen
-        name: 'Princes of the Yen', //navn
+         name: 'Princes of the Yen', //navn
         type: 'Film', //mediet, vælges imellem: 'Artikel', 'Bog', 'Film' og 'Podcast'
         rating: 5, //kvaliteten, tal mellem 1-10
         price: 0.00, //Pris for at tilgå referencen
@@ -144,9 +121,8 @@ var database = [
         description: 'Filmatisering af bogen - hvordan japanske centralbanker styrede den økonomiske udvikling i landet 1945-85, og hvordan det pga. overdreven pengeskabesle til fast ejendom og værdipapirer førte til en finansboble i slut-80"erne, med efterfølgende recession, som det tog landet 25 år at komme ud af', //Beskrivelse af den specifikke Artikel, bog osv.
         link: "https://www.youtube.com/watch?v=p5Ac7ap_MAY", //link 
         image: "Stockphoto film", //link til billede
-    },
-{
-        //Et eksempel
+    },{
+
         name: "Between Debt and Debt. Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
         type: "Artikel", //Kun et medie og skrevet med stort forbogstav
         rating: 10, //uden anførselstegn
@@ -158,116 +134,4 @@ var database = [
         //kun linje skift der hvor der står "<br>"
         link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
         image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-{        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-            {
-        //Et eksempel
-        name: "Demokratiser pengeskabelsen og indfri den grønne omstilling", //Bemærk det er skrevet mellem anførselstegnene
-        type: "Artikel", //Kun et medie og skrevet med stort forbogstav
-        rating: 10, //uden anførselstegn
-        price: 0.00, //med 2 decimaler, uden anførselstegn
-        date: "2021-11-29", //med anførselstegn
-        authors: "Mads Thunestvedt, Andrea Thorup", //komma sepererer forfattere, med anførselstegn om det hele
-        theme: ["Demokrati", "Grøn omstilling", "Pengeskabelse"], //om hvert emne er der anførselstegn
-        description: "Banker skaber penge – mange penge: Pengemængden stiger, og det har den de sidste mange årtier gjort med cirka 70 milliarder kroner om året, sidste år med hele 140 milliarder.<br>Banker er private virksomheder, og derfor udsteder de lån – altså penge – til formål, som er profitable og sikre for banken. Som oftest faste aktiver. Cirka 75 procent af udlån i danske banker er boligrelaterede. Meget få er til regenerativt jordbrug. <br> <br> Derfor burde vi spørge os selv om private virksomheder er et smart sted at placere kernen i den samfundskritiske institution,som pengesystemet er, nemlig pengeskabelsen. Og derfor er det essentielt, at vi begynder at inkludere pengeskabelsen i debatten om mulighederne for den grønne omstilling.", 
-        //kun linje skift der hvor der står "<br>"
-        link: "https://klimamonitor.dk/debat/art8482256/Demokratiser-pengeskabelsen-og-indfri-den-grønne-omstilling",
-        image: "https://images.squarespace-cdn.com/content/v1/587382daebbd1a717d8e9df3/7a3a4aff-519f-4d00-acd4-969b8503a575/Udklipklimademo.PNG?format=1000w",
-    },
-];
+    }, ];
